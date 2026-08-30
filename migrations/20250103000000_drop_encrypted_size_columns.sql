e57b27dac7724edddde4c7699ca0a486fe1ddac1
nynrathod <64688014+nynrathod@users.noreply.github.com>
2026-08-30 18:44:27 +0530

    chore(db): remove migration to drop file chunk size columns

--- migrations/20250103000000_drop_encrypted_size_columns.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE file_chunks
-  DROP COLUMN IF EXISTS encrypted_size,
-  DROP COLUMN IF EXISTS plaintext_size;
-
-ALTER TABLE file_chunks
-  DROP CONSTRAINT IF EXISTS encrypted_size_matches_plaintext;
